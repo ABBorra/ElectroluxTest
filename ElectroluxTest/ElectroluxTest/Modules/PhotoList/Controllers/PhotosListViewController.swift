1edf7e5d764f03c7210bb944539050f7ed15e107
AB_Borra <anjibabu@prestomall.com>
2022-10-30 06:02:49 +0800

    - UI Components Declaration in PhotoListVC    SearchBar    CollectionView    RefreshControl - pbXpro

--- ElectroluxTest/ElectroluxTest/Modules/PhotoList/Controllers/PhotosListViewController.swift
+++ ElectroluxTest/ElectroluxTest/Modules/PhotoList/Controllers/PhotosListViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class PhotosListViewController: UIViewController {
-
+    
+    // MARK: - Components
+    var photoCollectionView: UICollectionView!
+    let refreshControl = UIRefreshControl()
+    
+    /*SearchBar*/
+    lazy var photoSearchBar : UISearchBar = {
+        let photoSearch = UISearchBar()
+        photoSearch.placeholder = "Electrolux"
+        //photoSearch.delegate = self
+        photoSearch.tintColor = .gray
+        photoSearch.showsCancelButton = false
+        photoSearch.barStyle = .default
+        photoSearch.sizeToFit()
+        return photoSearch
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
