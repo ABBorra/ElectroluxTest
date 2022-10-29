2c714c6b7e5792c682db94692e497d74584f006f
AB_Borra <anjibabu@prestomall.com>
2022-10-30 05:34:42 +0800

    - Protocol to Fetch PhotosData using API from Flickr .Server.

---
+++ ElectroluxTest/ElectroluxTest/Library/APIRepository/PhotosApiRepository/PhotosApiRepository.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosApiRepository.swift
+//  ElectroluxTest
+//
+//  Created by AB_Borra on 10/30/22.
+//
+
+import Foundation
+
+protocol PhotosApiProtocol {
+    func getPhotosItems(pageSize: Int, page: Int, apiKey: String,searchImage: String,serviceName: String, onComplete: @escaping(PhotoListModel?) -> Void)
+}
+
+class PhotosApi: PhotosApiProtocol {
+    
+    // MARK: - Photos request from Api
+    func getPhotosItems(pageSize: Int, page: Int, apiKey: String,searchImage: String ,serviceName: String, onComplete: @escaping(PhotoListModel?) -> Void) {
+               let url = "\(ApiUrls.photosApiUrl)?api_key=\(apiKey)&method=\(serviceName)&per_page=\(pageSize)&page=\(page)&apiKey=\(apiKey)&tags=\(searchImage )&format=\("json")&nojsoncallback=\(true)&extras=\("media")&extras=\("url_sq")&extras=url_m"
+        
+        Request.get(str: url, type: PhotoListModel.self) { response, error in
+            onComplete(response)
+        }
+    }
+}
