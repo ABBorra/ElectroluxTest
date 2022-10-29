5368623360286e187f00152a1e28dffaec2e196c
AB_Borra <anjibabu@prestomall.com>
2022-10-30 04:27:31 +0800

    - Flickr Service ApiUrl.

---
+++ ElectroluxTest/ElectroluxTest/Library/APIRepository/ApiUrls.swift
@@ -0,0 +1,15 @@
+//
+//  ApiUrls.swift
+//  ElectroluxTest
+//
+//  Created by AB_Borra on 10/30/22.
+//
+
+import Foundation
+
+struct ApiUrls {
+    static var photosApiUrl: String {
+        return "https://api.flickr.com/services/rest"
+    }
+}
+
