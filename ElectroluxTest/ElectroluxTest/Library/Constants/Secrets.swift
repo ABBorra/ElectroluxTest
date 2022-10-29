cb0c8853ba5f71ea484dce6bbfb9b89d0f858b0e
AB_Borra <anjibabu@prestomall.com>
2022-10-30 04:22:39 +0800

    - Requested API_KEY from Flickr

---
+++ ElectroluxTest/ElectroluxTest/Library/Constants/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  ElectroluxTest
+//
+//  Created by AB_Borra on 10/30/22.
+//
+
+import Foundation
+
+class Secrets {
+    static var photoApiKey = "438dfde0cdddf298132410ed46ab4361"
+    static var secretKey = "944dc9a916c19afe"
+}
