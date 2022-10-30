90053efc83e5802b2d1e5af87cc5ba45e7ed13a3
AB_Borra <anjibabu@prestomall.com>
2022-10-30 17:58:41 +0800

    - UIComponents - DataBinding to elements - Components Constraints - Save Button

--- ElectroluxTest/ElectroluxTest/Modules/DetailedPhotoList/Controllers/PhotoDetailedViewController.swift
+++ ElectroluxTest/ElectroluxTest/Modules/DetailedPhotoList/Controllers/PhotoDetailedViewController.swift
@@ -36,29 +36,105 @@ class PhotoDetailedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        rightNavigationItem()
+        setupComponents()
     }
 
-    // MARK: - Setup NavigationItem
-    
-    func rightNavigationItem() {
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(addTapped))
+    // MARK: - Setup Save button
+    func saveImageButton() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveBtnAction))
         self.navigationItem.rightBarButtonItem?.tintColor = .black
     }
     
-    @objc func addTapped(sender: UIBarButtonItem) {
+    // MARK: - Save photo to Gallery
+    @objc func saveBtnAction(sender: UIBarButtonItem) {
+        let imageSaver = ImageSaver()
+        imageSaver.downloadToPhotoAlbum(image: self.userImage.image ?? UIImage())
+    }
+   
+    // MARK: - components
+    
+    fileprivate func setupComponents() {
+        saveImageButton()
+        self.addViews()
+    }
+    lazy var userImage: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.layer.cornerRadius = CGFloat(5)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    lazy var titleNameLbl: UILabel = {
+        return genericLbl(textfont: .boldSystemFont(ofSize: 18))
+    }()
+    
+    lazy var photoIdLbl: UILabel = {
+        return genericLbl(textfont: .systemFont(ofSize: 18))
+    }()
+    
+    lazy var ownerLbl: UILabel = {
+        return genericLbl(textfont: .systemFont(ofSize: 18))
+    }()
+    
+    // MARK: - setup genericLbl
+    func genericLbl(textfont: UIFont) -> UILabel {
+        let lbl = UILabel(frame: .zero)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        lbl.textColor = .black
+        lbl.font = textfont
+        lbl.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        return lbl
+    }
+    // MARK: - Adding SubViews
+    func addViews() {
+        userImage.loadImageWith(urlString: viewModel.imageURL ?? "")
+        titleNameLbl.text = "Title: \(viewModel.title ?? "")"
+        photoIdLbl.text = "PhotoID: \(viewModel.photoId ?? "")"
+        ownerLbl.text =  "OwnerID: \(viewModel.ownerId ?? "")"
         
+        self.view.addSubview(userImage)
+        self.view.addSubview(titleNameLbl)
+        self.view.addSubview(ownerLbl)
+        self.view.addSubview(photoIdLbl)
+        addConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - component Constraints
+    func addConstraints() {
+        
+        userImage.anchor(top: self.view.topAnchor,
+                         topConstant: navBarHeight + statusBarHeight,
+                         left: self.view.leftAnchor,
+                         leftConstant: 8,
+                         right: self.view.rightAnchor,
+                         rightConstant: 8,
+                         heightConstant: self.view.frame.height/2)
+        
+        titleNameLbl.anchor(top: userImage.bottomAnchor,
+                            topConstant: 10,
+                            left: self.view.leftAnchor,
+                            leftConstant: 8,
+                            right: self.view.rightAnchor,
+                            rightConstant: 8,
+                            heightConstant: 30,
+                            isHeightConstantLowPriority: true)
+       
+        ownerLbl.anchor(top: titleNameLbl.bottomAnchor,
+                        topConstant: 10,
+                        left: self.view.leftAnchor,
+                        leftConstant: 8,
+                        right: self.view.rightAnchor,
+                        rightConstant: 8,
+                        heightConstant: 20)
+        
+        photoIdLbl.anchor(top: ownerLbl.bottomAnchor,
+                          topConstant: 10,
+                          left: self.view.leftAnchor,
+                          leftConstant: 8,
+                          right: self.view.rightAnchor,
+                          rightConstant: 8,
+                          heightConstant: 20)
     }
-    */
-
 }
