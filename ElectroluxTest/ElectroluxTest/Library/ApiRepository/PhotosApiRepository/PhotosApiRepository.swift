df1961cde59d771e4682f7a2371cbabc9ac40140
AB_Borra <anjibabu@prestomall.com>
2022-10-31 12:05:12 +0800

    Url_Modified

--- ElectroluxTest/ElectroluxTest/Library/ApiRepository/PhotosApiRepository/PhotosApiRepository.swift
+++ ElectroluxTest/ElectroluxTest/Library/ApiRepository/PhotosApiRepository/PhotosApiRepository.swift
@@ -15,7 +15,7 @@ class PhotosApi: PhotosApiProtocol {
     
     // MARK: - Photos request from Api
     func getPhotosItems(pageSize: Int, page: Int, apiKey: String,searchImage: String ,serviceName: String, onComplete: @escaping(PhotoListModel?,Error?) -> Void) {
-               let url = "\(ApiUrls.photosApiUrl)?api_key=\(apiKey)&method=\(serviceName)&per_page=\(pageSize)&page=\(page)&apiKey=\(apiKey)&tags=\(searchImage )&format=\("json")&nojsoncallback=\(true)&extras=\("media")&extras=\("url_sq")&extras=url_m"
+               let url = "\(ApiUrls.photosApiUrl)?api_key=\(apiKey)&method=\(serviceName)&per_page=\(pageSize)&page=\(page)&tags=\(searchImage)&format=\("json")&nojsoncallback=\(true)&extras=\("media")&extras=\("url_sq")&extras=url_m"
         
         Request.get(str: url, type: PhotoListModel.self) { response, error in
             onComplete(response, error)
